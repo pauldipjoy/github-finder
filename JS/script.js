0959549fa5c0998df2ca25ba3f68baa25b4f25bb
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-02-27 16:36:28 +0600

    fetch api from user

---
+++ JS/script.js
@@ -0,0 +1,38 @@
+let searchButton = document.querySelector("#searchBtn");
+let searchUser = document.querySelector("#searchUser");
+
+
+searchButton.addEventListener("click" , (event) => {
+
+let userText = searchUser.value;
+
+if(userText != ''){
+
+    //alert(userText);
+
+    // Fetch API-
+
+    fetch(`https://api.github.com/users/${userText}`)
+
+    .then( result => result.json() )
+    .then(data => {
+
+
+        //console.log(data);
+
+        if(data.message == 'Not Found') {
+
+            //show alert
+        } else {
+
+            //show profile
+        }
+    })
+
+
+} else {
+
+    // clear profile
+}
+
+});
